75c91436655e0cd8741301d6749e8dc166a5dab6
danielaescutia07 <danielaescutia@yahoo.com>
2021-09-28 18:19:41 -0500

    mouseover complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+const nav = document.querySelector('nav');
+nav.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'gold';
+})
+// nav.addEventListener('mouseleave', function(event) {
+//     event.target.style.color = 'black';
+// })
+
