5d94922a9068c032afa17260a6300f8ff0d230cf
danielaescutia07 <danielaescutia@yahoo.com>
2021-09-28 22:16:11 -0500

    more events added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
 const nav = document.querySelector('nav');
 nav.addEventListener('mouseover', function(event) {
     event.target.style.color = 'gold';
-})
-// nav.addEventListener('mouseleave', function(event) {
-//     event.target.style.color = 'black';
-// })
+    setTimeout(function() {
+        event.target.style.color = '';
+    }, 500);
+}, false);
 
+
+const busImage = document.querySelector('.intro img');
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 65) {
+        busImage.style.borderColor = '#264653'
+        busImage.style.borderWidth = '10px'
+        busImage.style.borderStyle = 'double'
+    }
+})
+
+
+document.addEventListener('keyup', function(event) {
+    if (event.keyCode === 65) {
+        busImage.style.borderStyle = 'none'
+    }
+})
+
+document.addEventListener('wheel', function(event) {
+    console.log(`You are scrolling using ${event}`)
+})
